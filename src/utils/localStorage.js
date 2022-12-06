f5cff882015e744b8b5e061516a53297eedbbbff
Kevin Terrier <kevin.terrier@le1817.com>
2022-12-06 10:19:28 +0100

    added localstorage

---
+++ src/utils/localStorage.js
@@ -0,0 +1,40 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+export const storeData = async (key, value) => {
+    try {
+        await AsyncStorage.setItem(key, value)
+    } catch (e) {
+        alert(e)
+    }
+}
+
+export const getData = async (key) => {
+    try {
+        const value = await AsyncStorage.getItem(key)
+        if (value !== null) {
+            return value
+        }
+    } catch (e) {
+        alert(e)
+    }
+}
+
+removeValue = async (key) => {
+    try {
+        await AsyncStorage.removeItem(key)
+    } catch (e) {
+        // remove error
+    }
+
+    console.log('Done.')
+}
+
+clearAll = async () => {
+    try {
+        await AsyncStorage.clear()
+    } catch (e) {
+        // clear error
+    }
+
+    console.log('Done.')
+}
